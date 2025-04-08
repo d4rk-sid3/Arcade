013fb2dff5765c803b0acbbfe82d153e8bd04d5e
amour <amour.guidi@epitech.eu>
2025-04-08 10:11:38 +0100

    [Feat]: adding of new functionality to the nibbler

--- nibbler.cpp
+++ nibbler.cpp
@@ -5,3 +5,288 @@
 ** nibbler
 */
 
+#include "nibbler.hpp"
+
+std::string readFile(const std::string& filename)
+{  
+    std::ifstream file(filename);  
+    if (!file) {  
+        std::cerr << "Error opening file: " << filename << std::endl;  
+        return "";  
+    }  
+    std::string content;  
+    std::string line;
+
+    while (std::getline(file, line)) {  
+        content += line + "\n";
+    }  
+    return content;  
+}  
+
+std::vector<std::string> readtable(const std::string& m)
+{
+    std::vector<std::string> newmap;
+    std::istringstream stream(m);
+    std::string line;
+    
+    while (std::getline(stream, line)) {
+        newmap.push_back(line);
+    }
+    return newmap;
+}
+
+nibbler_l::nibbler_l(int w, int h, const std::string& m): d('D'), x(w / 2), y(h / 2), x_food(0), y_food(0), map(readtable(m)), nb_fruit(12), score(0)
+{
+    for (int i = 0; i < 4; i++) {
+        nibbler.push_back({x, y - i});  
+        map[y][x] = 'O';
+    }
+    eatfood();
+}
+
+void nibbler_l::eatfood()
+{  
+    std::srand(std::time(nullptr));
+
+    x_food = x;
+    y_food = y;
+    int len_x = map[0].size();
+    int len_y = map.size();
+    int a = 0;
+    while (a < nb_fruit) {
+        if (map[y_food][x_food] == ' ') {
+            map[y_food][x_food] = 'X';
+            foods.push_back({x_food, y_food});
+            a++;
+        }
+        x_food = std::rand() % len_x;  
+        y_food = std::rand() % len_y;
+    }
+}
+
+void nibbler_l::direction(char newDirection)
+{  
+    if ((d == 'U' && newDirection != 'D') ||   
+        (d == 'D' && newDirection != 'U') ||   
+        (d == 'L' && newDirection != 'R') ||   
+        (d == 'R' && newDirection != 'L')) {  
+        d = newDirection;
+    }  
+}  
+
+bool nibbler_l::move()
+{  
+    auto head = nibbler.front();  
+    int new_x = head.first;  
+    int new_y = head.second;  
+
+    if (d == 'U') {
+        new_y--;
+    } else if (d == 'D') {
+        new_y++;
+    } else if (d == 'L') {
+        new_x--;
+    } else if (d == 'R') {
+        new_x++;
+    }
+
+    if (map[new_y][new_x] == '#') {
+        return true;
+    }
+
+    for (size_t i = 1; i < nibbler.size(); ++i) {  
+        if (nibbler[i] == std::make_pair(new_x, new_y)) {  
+            return false;
+        }  
+    }
+    if (!nibbler.empty()) {
+        map[head.second][head.first] = 'B';
+    }
+
+    nibbler.insert(nibbler.begin(), {new_x, new_y});
+    map[new_y][new_x] = 'O';
+
+    for (int i = 0; i < foods.size(); i++) {
+        if (foods[i] == std::make_pair(new_x, new_y)) {
+            score += 10;
+            std::cout << "Score: " << score << std::endl;
+            return true;
+        }
+    }
+    auto tail = nibbler.back();  
+    map[tail.second][tail.first] = ' '; 
+    nibbler.pop_back();
+
+    return true;  
+}  
+
+const std::vector<std::string>& nibbler_l::getMap() const
+{  
+    return map;  
+}   
+
+nibbler_l::~nibbler_l()
+{
+
+}
+
+void nibbler_display::create_head()
+{
+    if (!head_texture.loadFromFile("head.png")) {
+        std::cerr << "Error loading background image" << std::endl;
+        return;
+    }
+
+    float scale = 20.0f / 1024.0f;
+    head_sprite.setTexture(head_texture);
+    head_sprite.setScale(scale, scale);
+}
+
+void nibbler_display::create_blob()
+{
+    if (!blob_texture.loadFromFile("blob.png")) {
+        std::cerr << "Error loading background image" << std::endl;
+        return;
+    }
+
+    float scale = 20.0f / 512.0f;
+    blob_sprite.setTexture(blob_texture);
+    blob_sprite.setScale(scale, scale);
+}
+
+void nibbler_display::create_wall()
+{
+    if (!wall_texture.loadFromFile("wall.png")) {
+        std::cerr << "Error loading background image" << std::endl;
+        return;
+    }
+
+    float scale = 20.0f / 216.0f;
+    wall_sprite.setTexture(wall_texture);
+    wall_sprite.setScale(scale, scale);
+}
+
+void nibbler_display::create_death()
+{
+    if (!death_texture.loadFromFile("xx.png")) {
+        std::cerr << "Error loading background image" << std::endl;
+        return;
+    }
+
+    float scale = 20.0f / 1024.0f;
+    death_sprite.setTexture(death_texture);
+    death_sprite.setScale(scale, scale);
+}
+
+void nibbler_display::create_apple()
+{
+    if (!apple_texture.loadFromFile("apple.png")) {
+        std::cerr << "Error loading background image" << std::endl;
+        return;
+    }
+
+    float scale = 20.0f / 1024.0f;
+    apple_sprite.setTexture(apple_texture);
+    apple_sprite.setScale(scale, scale);
+}
+
+nibbler_display::nibbler_display(nibbler_l &l): logic(l), window(sf::VideoMode(963, 600), "nibbler Game")
+{
+    create_head();
+    create_blob();
+    create_wall();
+    create_death();
+    create_apple();
+}
+
+void nibbler_display::key_input()
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+        logic.direction('U');
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+        logic.direction('D');
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+        logic.direction('L');
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+        logic.direction('R');
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
+        window.close();
+    }
+}
+
+void nibbler_display::move_nibbler(sf::Clock *clock)
+{
+    if ((*clock).getElapsedTime().asMilliseconds() >= 150) {  
+        if (!logic.move()) {  
+            std::cout << "Game Over!" << std::endl;  
+            window.close();  
+        }  
+        (*clock).restart();
+    }
+}
+
+void nibbler_display::draw()
+{
+    window.setFramerateLimit(10);  
+    const auto& map = logic.getMap();  
+    int cellSize = 20; 
+    sf::Clock clock;
+    sf::RectangleShape cell(sf::Vector2f(cellSize, cellSize));
+
+    while (window.isOpen()) {  
+        sf::Event event;  
+        while (window.pollEvent(event)) {  
+            if (event.type == sf::Event::Closed) {  
+                window.close();  
+            }
+        }
+        key_input();
+        move_nibbler(&clock);
+         
+        window.clear();  
+        for (size_t y = 0; y < map.size(); ++y) {  
+            for (size_t x = 0; x < map[y].size(); ++x) {  
+
+                if (map[y][x] == 'O') {
+                    head_sprite.setPosition(x * cellSize, y * cellSize);
+                    window.draw(head_sprite); 
+                } else if (map[y][x] == 'X') {  
+                    apple_sprite.setPosition(x * cellSize, y * cellSize);
+                    window.draw(apple_sprite);
+                } else if (map[y][x] == '#'){  
+                    wall_sprite.setPosition(x * cellSize, y * cellSize);
+                    window.draw(wall_sprite);
+                }else if (map[y][x] == 'B') {
+                    blob_sprite.setPosition(x * cellSize, y * cellSize);
+                    window.draw(blob_sprite);
+                } else {
+                    cell.setPosition(x * cellSize, y * cellSize);
+                    cell.setFillColor(sf::Color::Black);
+                    window.draw(cell);
+                }
+            }  
+        }  
+        window.display();
+    }  
+}
+
+nibbler_display::~nibbler_display()
+{
+
+}
+
+int main() {  
+    const int width = 49;
+    const int height = 31;  
+
+    std::string mapcontent = readFile("example.txt");
+    nibbler_l logic(width, height, mapcontent);
+    nibbler_display display(logic);
+    display.draw();
+
+    return 0;  
+}
--- nibbler.hpp
+++ nibbler.hpp
@@ -5,43 +5,70 @@
 ** nibbler
 */
 
-#ifndef NIBBLER_HPP_
-#define NIBBLER_HPP_
+#ifndef nibbler_HPP_
+#define nibbler_HPP_
 
     #include <SFML/Graphics.hpp>  
-    #include <iostream>  
+    #include <iostream>
+    #include <fstream>
     #include <vector>  
     #include <ctime>  
-    #include <cstdlib>  
+    #include <cstdlib>
+    #include <sstream>
     #include <thread>
 
     class nibbler_l {
         private:
             std::vector<std::string> map;
-            std::vector<std::pair<int, int>> snake;
+            std::vector<std::pair<int, int>> nibbler;
             char d;
             int x_food, y_food;
             int x, y;
+            int nb_fruit;
+            int score;
+            std::vector <std::pair<int, int>> foods;
+
 
         public:
-            nibbler_l(int w, int h);
+            nibbler_l(int w, int h, const std::string& m);
             ~nibbler_l();
+            void eatfood();
             void direction(char newDirection);
             bool move();
+            bool new_direction(int new_x, int new_y);
             const std::vector<std::string>& getMap() const;
-            void food(int count);
+
     };
 
-    class nibbler_display {
+    class nibbler_display {   
         private:
-            nibbler_l & logique;
+            nibbler_l &logic;
+            sf::RenderWindow window;
+            sf::Event event;
+            sf::Texture head_texture;
+            sf::Texture blob_texture;
+            sf::Texture wall_texture;
+            sf::Texture death_texture;
+            sf::Texture apple_texture;
+            sf::Sprite head_sprite;
+            sf::Sprite blob_sprite;
+            sf::Sprite wall_sprite;
+            sf::Sprite death_sprite;
+            sf::Sprite apple_sprite;
+
 
         public:
-            nibbler_display();
-            ~nibbler_display();
+            nibbler_display(nibbler_l &l);
             void draw();
+            ~nibbler_display();
             void key_input();
-            void move_snake(sf::Clock *clock);   
+            void move_nibbler(sf::Clock *clock);
+            void create_head();
+            void create_blob();
+            void create_wall();
+            void create_death();
+            void create_apple();
+
     };
 
-#endif /* !NIBBLER_HPP_ */
+#endif /* !nibbler_HPP_ */
